9ade455b282cd175e091f25690c307f07990955b
Dennis Leon <leonde@vmware.com>
2021-06-11 15:51:11 -0700

    WIP
    Authored-by: Dennis Leon <leonde@vmware.com>

--- hack/build-binaries.sh
+++ hack/build-binaries.sh
@@ -2,14 +2,19 @@
 
 set -e -x -u
 
-./hack/build.sh
+LATEST_GIT_TAG=$(git describe --tags | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+VERSION="${1:-$LATEST_GIT_TAG}"
+
+go fmt ./cmd/... ./pkg/... ./test/...
+go mod vendor
+go mod tidy
 
 # makes builds reproducible
 export CGO_ENABLED=0
-repro_flags="-ldflags=-buildid= -trimpath"
+LDFLAGS="-X github.com/k14s/kbld/pkg/kbld/version.Version=$VERSION -buildid="
 
-GOOS=darwin GOARCH=amd64 go build $repro_flags -o kbld-darwin-amd64 ./cmd/kbld/...
-GOOS=linux GOARCH=amd64 go build $repro_flags -o kbld-linux-amd64 ./cmd/kbld/...
-GOOS=windows GOARCH=amd64 go build $repro_flags -o kbld-windows-amd64.exe ./cmd/kbld/...
+GOOS=darwin GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o kbld-darwin-amd64 ./cmd/kbld/...
+GOOS=linux GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o kbld-linux-amd64 ./cmd/kbld/...
+GOOS=windows GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o kbld-windows-amd64.exe ./cmd/kbld/...
 
-shasum -a 256 ./kbld-*-amd64*
+shasum -a 256 ./kbld-darwin-amd64 ./kbld-linux-amd64 ./kbld-windows-amd64.exe
